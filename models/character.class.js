318c1934ad7b14090d67e5ee0183123b1a61be57
Galina Antipin <galina.antipin@gmail.com>
2024-08-26 14:20:01 +0200

    added sound

--- models/character.class.js
+++ models/character.class.js
@@ -12,6 +12,7 @@ class Character extends MovableObject {
     ];
 
     world;
+    walking_sound = new Audio('audio/running.mp3');
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
@@ -22,14 +23,16 @@ class Character extends MovableObject {
 
     animate() {
         setInterval(() => {
+            this.walking_sound.pause();
             if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.x +=this.speed;
-                this.otherDirection = false;
+                this.otherDirection = false;  
+                this.walking_sound.play();
             }
 
             if (this.world.keyboard.LEFT && this.x > 0 ) {
                 this.x -=this.speed;
-                this.otherDirection = true;
+                this.otherDirection = true; 
             }
             this.world.camera_x = -this.x +100;
         }, 1000 / 60);
