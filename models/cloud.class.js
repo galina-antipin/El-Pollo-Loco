5a2f5d2b723626672ed47bbca570c1a9daddc886
Galina Antipin <galina.antipin@gmail.com>
2024-08-26 10:48:29 +0200

    added background img

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -3,8 +3,15 @@ class Cloud extends MovableObject {
     width = 500;
     height = 250;
 
-    constructor(){
+    constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
-        this.x = 0 + Math.random()*500;
+        this.x = Math.random() * 500;
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.x -= 0.15;
+        }, 1000 / 60)
     }
 }
