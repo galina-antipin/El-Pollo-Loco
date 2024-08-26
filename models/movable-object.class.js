081a0a00d49ef123f38ee38be782edc89bbd4700
Galina Antipin <galina.antipin@gmail.com>
2024-08-26 10:06:28 +0200

    objects position

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,6 +1,6 @@
 class MovableObject {
     x = 120;
-    y = 250;
+    y = 320;
     img;
     height = 150;
     width = 100;
--- models/world.class.js
+++ models/world.class.js
@@ -10,7 +10,9 @@ class World {
         new Cloud()
     ];
     backgroundObjects = [
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png')
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
     ]
     canvas;
     ctx;
@@ -22,22 +24,22 @@ class World {
 
     draw() {
         this.ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+        this.addObjectsToMap(this.backgroundObjects);
         this.addToMap(this.character);
         this.addObjectsToMap(this.clouds);
         this.addObjectsToMap(this.enemies);
-        this.addObjectsToMap(this.backgroundObjects);
+
 
         let self = this;
         requestAnimationFrame(function () {
             self.draw();
         });
     }
-addObjectsToMap(objects){
-    objects.forEach(o => {
-        this.addToMap(o);
-    });
-}
+    addObjectsToMap(objects) {
+        objects.forEach(o => {
+            this.addToMap(o);
+        });
+    }
     addToMap(mo) {
         this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
     }
