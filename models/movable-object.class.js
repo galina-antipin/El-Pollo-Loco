20337b51cb0ea92f3732bd877f26847d79a7cfc0
Galina Antipin <galina.antipin@gmail.com>
2024-08-27 10:06:46 +0200

    some new folder

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -10,6 +10,9 @@ class MovableObject {
     otherDirection = false;
     speedY = 0;
     acceleration = 2.5;
+    energy = 100;
+
+    lastHit = 0;
 
     applyGravity() {
         setInterval(() => {
@@ -20,13 +23,55 @@ class MovableObject {
         }, 1000 / 25);
     }
 
-    isAboveGround(){
+    isAboveGround() {
         return this.y < 180;
     }
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    drawFrame(ctx) {
+        if (this instanceof Character || this instanceof Chicken) {
+            ctx.beginPath();
+            ctx.lineWidth = '5';
+            ctx.strokeStyle = 'blue';
+            ctx.rect(this.x, this.y, this.width, this.height);
+            ctx.stroke();
+        }
+    }
+
+    isColliding(mo) {
+        return (this.x < (mo.x + mo.width) &&
+            (this.x + this.width) > mo.x &&
+            this.y < (mo.y + mo.height) &&
+            (this.y + this.height) > mo.y);
+    }
+
+    hit() {
+        this.energy -= 5;
+        if (this.energy < 0) {
+            this.energy = 0;
+
+        } else {
+            this.lastHit = new Date().getTime();
+
+        }
+    }
+
+    isHurt() {
+        let timepassed = new Date().getTime() - this.lastHit;
+        timepassed = timepassed / 1000;
+        return timepassed < 1;
+    }
+
+    isDead() {
+        return this.energy == 0;
     }
 
     loadImages(arr) {
@@ -38,7 +83,7 @@ class MovableObject {
     }
 
     playAnimation(images) {
-        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let i = this.currentImage % images.length;
         let path = images[i];
         this.img = this.imageCache[path];
         this.currentImage++;
@@ -49,10 +94,10 @@ class MovableObject {
     }
 
     moveLeft() {
-            this.x -= this.speed;
+        this.x -= this.speed;
     }
 
-    jump(){
+    jump() {
         this.speedY = 30;
     }
 }
