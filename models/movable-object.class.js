aa0cf44bdfcec3d18846422213c76fe3c66c2400
Galina Antipin <galina.antipin@gmail.com>
2024-08-27 12:13:43 +0200

    added font and background image

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,11 +1,4 @@
-class MovableObject {
-    x = 120;
-    y = 320;
-    img;
-    height = 150;
-    width = 100;
-    imageCache = {};
-    currentImage = 0;
+class MovableObject extends DrawableObject {
     speed = 0.15;
     otherDirection = false;
     speedY = 0;
@@ -24,27 +17,14 @@ class MovableObject {
     }
 
     isAboveGround() {
+        if (this instanceof ThrowableObject) {
+            return true;
+        } else {
         return this.y < 180;
-    }
+    }}
 
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
 
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
 
-    drawFrame(ctx) {
-        if (this instanceof Character || this instanceof Chicken) {
-            ctx.beginPath();
-            ctx.lineWidth = '5';
-            ctx.strokeStyle = 'blue';
-            ctx.rect(this.x, this.y, this.width, this.height);
-            ctx.stroke();
-        }
-    }
 
     isColliding(mo) {
         return (this.x < (mo.x + mo.width) &&
@@ -72,14 +52,6 @@ class MovableObject {
 
     isDead() {
         return this.energy == 0;
-    }
-
-    loadImages(arr) {
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
     }
 
     playAnimation(images) {
